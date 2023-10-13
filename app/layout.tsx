786f7549dea2f52d6d49dd42f68ab57a0fc213df
maymyintmyat <stallam23@gmail.com>
2023-10-13 21:46:52 +0630

    fixed commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+export const metadata = {
+  title: "Weather App 🌤",
+  description: "Weather app",
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={poppins.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import React, { useState } from "react";
+import Current from "./components/current";
+import Input from "./components/input";
+import WeatherDetails from "./components/weatherDetails";
+import WeekForecast from "./components/weekForecast";
+
+export default function Home() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=5fab3d789f7a443a902133934231310&q=${location}&days=7&aqi=no&alerts=no
+  `;
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+        setLocation("");
+        setError("");
+      } catch (error) {
+        setError("City not found");
+        setData({});
+      }
+    }
+  };
+
+  let content;
+  if (Object.keys(data).length === 0 && error === "") {
+    content = (
+      <div className="text-white text-center h-screen mt-[5rem]">
+        <h2 className="text-3xl font-semibold mb-4">
+          Welcome to the Weather App
+        </h2>
+        <p className="text-xl">Enter a city name to get the weather forecast</p>
+      </div>
+    );
+  } else if (error !== "") {
+    content = (
+      <div className="text-white text-center h-screen mt-[5rem]">
+        <h2 className="text-3xl font-semibold mb-4">City not found</h2>
+        <p className="text-xl">Please enter a valid city name</p>
+      </div>
+    );
+  } else {
+    content = (
+      <>
+        <div className="flex md:flex-row flex-col p-12 items-center justify-between mt-[-4rem] gap-10">
+          <Current data={data} />
+          <WeekForecast data={data} />
+        </div>
+        <div>
+          <WeatherDetails data={data} />
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-fit">
+      <div className="bg-white/25 w-full rounded-lg flex flex-col h-fit">
+        <div className="flex flex-col md:flex-row justify-between items-center p-12">
+          <Input handleSearch={handleSearch} setLocation={setLocation} />
+          <h1 className="mb-8 md:mb-0 order-1 text-white text-2xl py-2 px-4 rounded-xl italic font-bold">
+            Weather App.
+          </h1>
+        </div>
+        {content}
+      </div>
+    </div>
+  );
+}
